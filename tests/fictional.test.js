b3c9563cfa3359f4ad6807f5201c74be96b303a5
Justin van der Merwe <justin.vandermerwe@researchgate.net>
2020-04-22 21:18:08 +0200

    feat(int): Support basic int() type

--- tests/fictional.test.js
+++ tests/fictional.test.js
@@ -1,3 +1,34 @@
+const fictional = require('..')
 const tap = require('tap')
 
-tap.equal(true, true)
+const types = getTypes()
+
+tap.test('generated values', t => {
+  t.matchSnapshot(callTypes(23))
+  t.end()
+})
+
+tap.test('consistency', t => {
+  let i = -1
+  const firstResult = callTypes(23)
+
+  while (++i < 100) {
+    t.deepEquals(callTypes(23), firstResult)
+  }
+
+  t.end()
+})
+
+function callTypes(input) {
+  const result = {}
+
+  for (const name of Object.keys(types)) {
+    result[name] = types[name](input)
+  }
+
+  return result
+}
+
+function getTypes() {
+  return fictional
+}
