31acd7cd4a3b0d1b81e921c14f8b293faa7b4a25
Justin van der Merwe <justnvdm@gmail.com>
2022-12-07 22:48:49 +0200

    Try out blake2

--- hash.js
+++ hash.js
@@ -1,11 +1,15 @@
-var { fast1a52 } = require('fnv-plus')
+var { blake2b } = require('blakejs')
 var stringify = require('fast-json-stable-stringify')
 var createLimitCache = require('./utils/createLimitCache')
+
+const NUM_BITS = 24
+const NUM_BYTES = Math.floor(NUM_BITS / 8)
 
 var cache = createLimitCache(100)
 
 var hash = cache.memoize(function hashFn(input) {
-  return fast1a52(stringify(input) + hash.salt)
+  var key = stringify(input) + hash.salt
+  return uint8ArrayToNumber(blake2b(key, null, NUM_BYTES))
 })
 
 hash.salt = 'chino'
@@ -16,6 +20,17 @@ hash.hash2 = function hash2(a, b) {
 
 hash.hash3 = function hash3(a, b, c) {
   return hash(a) + hash(b) + hash(c)
+}
+
+const uint8ArrayToNumber = bytes => {
+  let result = 0
+  let i = bytes.length
+
+  while (i--) {
+    result = (result << 8) + bytes[i]
+  }
+
+  return result
 }
 
 module.exports = hash
