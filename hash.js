c88366d758a1b8f3ad183d1c3353b8e9ba3c53b2
Justin van der Merwe <justnvdm@gmail.com>
2022-12-07 22:48:56 +0200

    Revert "Try out blake2"
    This reverts commit 31acd7cd4a3b0d1b81e921c14f8b293faa7b4a25.

--- hash.js
+++ hash.js
@@ -1,15 +1,11 @@
-var { blake2b } = require('blakejs')
+var { fast1a52 } = require('fnv-plus')
 var stringify = require('fast-json-stable-stringify')
 var createLimitCache = require('./utils/createLimitCache')
-
-const NUM_BITS = 24
-const NUM_BYTES = Math.floor(NUM_BITS / 8)
 
 var cache = createLimitCache(100)
 
 var hash = cache.memoize(function hashFn(input) {
-  var key = stringify(input) + hash.salt
-  return uint8ArrayToNumber(blake2b(key, null, NUM_BYTES))
+  return fast1a52(stringify(input) + hash.salt)
 })
 
 hash.salt = 'chino'
@@ -20,17 +16,6 @@ hash.hash2 = function hash2(a, b) {
 
 hash.hash3 = function hash3(a, b, c) {
   return hash(a) + hash(b) + hash(c)
-}
-
-const uint8ArrayToNumber = bytes => {
-  let result = 0
-  let i = bytes.length
-
-  while (i--) {
-    result = (result << 8) + bytes[i]
-  }
-
-  return result
 }
 
 module.exports = hash
