f29fa03b01ba880d0185912bee2002f16853c233
Justin van der Merwe <justnvdm@gmail.com>
2022-12-11 12:12:17 +0200

    Change to using siphash

--- hash.js
+++ hash.js
@@ -1,14 +1,16 @@
-var { fast1a52 } = require('fnv-plus')
+var siphash = require('sip-hash')
 var stringify = require('fast-json-stable-stringify')
 var createLimitCache = require('./utils/createLimitCache')
 
 var cache = createLimitCache(100)
 
 var hash = cache.memoize(function hashFn(input) {
-  return fast1a52(stringify(input) + hash.salt)
+  return siphash.hash_uint(hash.key, stringify(input))
 })
 
-hash.salt = 'chino'
+hash.generateKey = siphash.string16_to_key.bind(siphash)
+
+hash.key = hash.generateKey('chino')
 
 hash.hash2 = function hash2(a, b) {
   return hash(a) + hash(b)
